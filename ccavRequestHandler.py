e2790fb39b697dc34670e4bdb371ab879f929b87
AmritSingh <amrit.s@ekoinfomatics.com>
2023-06-06 17:06:04 +0530

    reponse to request

--- ccavRequestHandler.py
+++ ccavRequestHandler.py
@@ -51,13 +51,13 @@ def login():
 	# p_merchant_param3 = request.form['merchant_param3']
 	# p_merchant_param4 = request.form['merchant_param4']
 	# p_merchant_param5 = request.form['merchant_param5']
-	# p_integration_type = request.form['integration_type']
+	p_integration_type = "iframe_normal"
 	# p_promo_code = request.form['promo_code']
 	# p_customer_identifier = request.form['customer_identifier']
 	
 	
 
-	merchant_data='merchant_id='+p_merchant_id+'&'+'order_id='+p_order_id + '&' + "currency=" + p_currency + '&' + 'amount=' + p_amount+'&'+'redirect_url='+p_redirect_url+'&'+'cancel_url='+p_cancel_url+'&'+'language='+p_language
+	merchant_data='merchant_id='+p_merchant_id+'&'+'order_id='+p_order_id + '&' + "currency=" + p_currency + '&' + 'amount=' + p_amount+'&'+'redirect_url='+p_redirect_url+'&'+'cancel_url='+p_cancel_url+'&'+'language='+p_language+'&integration_type='+p_integration_type
     # +'&'+'billing_name='+p_billing_name+'&'+'billing_address='+p_billing_address+'&'+'billing_city='+p_billing_city+'&'+'billing_state='+p_billing_state+'&'+'billing_zip='+p_billing_zip+'&'+'billing_country='+p_billing_country+'&'+'billing_tel='+p_billing_tel+'&'+'billing_email='+p_billing_email+'&'+'delivery_name='+p_delivery_name+'&'+'delivery_address='+p_delivery_address+'&'+'delivery_city='+p_delivery_city+'&'+'delivery_state='+p_delivery_state+'&'+'delivery_zip='+p_delivery_zip+'&'+'delivery_country='+p_delivery_country+'&'+'delivery_tel='+p_delivery_tel+'&'+'merchant_param1='+p_merchant_param1+'&'+'merchant_param2='+p_merchant_param2+'&'+'merchant_param3='+p_merchant_param3+'&'+'merchant_param4='+p_merchant_param4+'&'+'merchant_param5='+p_merchant_param5+'&'+'integration_type='+p_integration_type+'&'+'promo_code='+p_promo_code+'&'+'customer_identifier='+p_customer_identifier+'&'
 	
 	encryption = encrypt(merchant_data,workingKey)
@@ -72,7 +72,8 @@ def login():
     <center>
 	<!-- width required mininmum 482px -->
        	<iframe  width="482" height="500" scrolling="No" frameborder="0"  id="paymentFrame" src="https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction&merchant_id=$mid&encRequest=$encReq&access_code=$xscode">
-	  	</iframe>
+	  	
+          </iframe>
 	</center>
 	
 	<script type="text/javascript">
